ca85b261d28a501c9363b74a4b1b7328d9911cb8
Hubert Michalski <s28546@pjwstk.edu.pl>
2024-03-11 12:53:41 +0100

    GetMax

--- console-app/Program.cs
+++ console-app/Program.cs
@@ -4,6 +4,17 @@ namespace console_app
 {
     internal class Program
     {
+        public static int GetMax(int[] arr)
+        {
+            int max = Int32.MinValue;
+            foreach (int num in arr)
+            {
+                if (num > max)
+                    max = num;
+            }
+            return max;
+        }
+        
         public static float GetAverage(int[] arr)
         {
             int sum = 0;

aa6bb09be3c146260b720c5d7712f6fd8b5d06e8
Hubert Michalski <s28546@pjwstk.edu.pl>
2024-03-11 12:40:20 +0100

    GetAverage

--- console-app/Program.cs
+++ console-app/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace console_app
+﻿using System;
+
+namespace console_app
 {
     internal class Program
     {
+        public static float GetAverage(int[] arr)
+        {
+            int sum = 0;
+            foreach (int num in arr)
+            {
+                sum += num;
+            }
+
+            return ((float) sum )/ arr.Length;
+        }
+        
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
         }
     }
 }
